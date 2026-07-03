d0a8bc11d2cf434a1c4890db8b92b8b71ff0fa79
ikiraihan <ikiraihann@gmail.com>
2026-07-03 19:22:25 +0700

    fix export

--- app/Exports/RegistrationClassExport.php
+++ app/Exports/RegistrationClassExport.php
@@ -38,126 +38,126 @@ class RegistrationClassExport implements FromCollection, WithHeadings, WithMappi
             $query->where('event_id', $eventId);
         }
 
-        if (!empty($this->filters['event_class_id'])) {
-            $query->where('event_class_id', $this->filters['event_class_id']);
-        }
+        // if (!empty($this->filters['event_class_id'])) {
+        //     $query->where('event_class_id', $this->filters['event_class_id']);
+        // }
 
-        if (!empty($this->filters['search'])) {
+        // if (!empty($this->filters['search'])) {
 
-            $search = $this->filters['search'];
+        //     $search = $this->filters['search'];
 
-            $query->where(function ($q) use ($search) {
+        //     $query->where(function ($q) use ($search) {
 
-                $q->where('invoice_number', 'like', "%{$search}%")
-                    ->orWhere('racer_number', 'like', "%{$search}%")
-                    ->orWhere('vehicle', 'like', "%{$search}%")
-                    ->orWhereHas('registration', function ($registration) use ($search) {
-                        $registration->where('team_name', 'like', "%{$search}%");
-                    })
-                    ->orWhereHas('registration.racer', function ($racer) use ($search) {
-                        $racer->where('full_name', 'like', "%{$search}%")
-                            ->orWhere('nik', 'like', "%{$search}%");
-                    });
+        //         $q->where('invoice_number', 'like', "%{$search}%")
+        //             ->orWhere('racer_number', 'like', "%{$search}%")
+        //             ->orWhere('vehicle', 'like', "%{$search}%")
+        //             ->orWhereHas('registration', function ($registration) use ($search) {
+        //                 $registration->where('team_name', 'like', "%{$search}%");
+        //             })
+        //             ->orWhereHas('registration.racer', function ($racer) use ($search) {
+        //                 $racer->where('full_name', 'like', "%{$search}%")
+        //                     ->orWhere('nik', 'like', "%{$search}%");
+        //             });
 
-            });
-        }
+        //     });
+        // }
 
-        $query->when($this->filters['race_racer_number'] ?? null,
-            fn($q, $v) => $q->whereHas('registration', fn($registration) => $registration->where('racer_number', 'like', "%{$v}%")));
+        // $query->when($this->filters['race_racer_number'] ?? null,
+        //     fn($q, $v) => $q->whereHas('registration', fn($registration) => $registration->where('racer_number', 'like', "%{$v}%")));
 
-        $query->when($this->filters['race_vehicle'] ?? null,
-            fn($q, $v) => $q->where('vehicle', 'like', "%{$v}%"));
+        // $query->when($this->filters['race_vehicle'] ?? null,
+        //     fn($q, $v) => $q->where('vehicle', 'like', "%{$v}%"));
 
-        $query->when($this->filters['race_chassis_number'] ?? null,
-            fn($q, $v) => $q->where('rangka_number', 'like', "%{$v}%"));
+        // $query->when($this->filters['race_chassis_number'] ?? null,
+        //     fn($q, $v) => $q->where('rangka_number', 'like', "%{$v}%"));
 
-        $query->when($this->filters['race_engine_number'] ?? null,
-            fn($q, $v) => $q->where('vehicle_number', 'like', "%{$v}%"));
+        // $query->when($this->filters['race_engine_number'] ?? null,
+        //     fn($q, $v) => $q->where('vehicle_number', 'like', "%{$v}%"));
 
-        $query->when(
-            ($this->filters['race_racer_name'] ?? null)
-            || ($this->filters['race_nik'] ?? null)
-            || ($this->filters['race_city'] ?? null)
-            || (($this->filters['race_has_photo'] ?? '') !== '')
-            || (($this->filters['race_has_kis'] ?? '') !== '')
-            || (($this->filters['race_has_kta'] ?? '') !== ''),
-            function ($q) {
+        // $query->when(
+        //     ($this->filters['race_racer_name'] ?? null)
+        //     || ($this->filters['race_nik'] ?? null)
+        //     || ($this->filters['race_city'] ?? null)
+        //     || (($this->filters['race_has_photo'] ?? '') !== '')
+        //     || (($this->filters['race_has_kis'] ?? '') !== '')
+        //     || (($this->filters['race_has_kta'] ?? '') !== ''),
+        //     function ($q) {
 
-                $q->whereHas('registration.racer', function ($racer) {
+        //         $q->whereHas('registration.racer', function ($racer) {
 
-                    if (!empty($this->filters['race_racer_name'])) {
-                        $racer->where('full_name', 'like', '%' . $this->filters['race_racer_name'] . '%');
-                    }
+        //             if (!empty($this->filters['race_racer_name'])) {
+        //                 $racer->where('full_name', 'like', '%' . $this->filters['race_racer_name'] . '%');
+        //             }
 
-                    if (!empty($this->filters['race_nik'])) {
-                        $racer->where('nik', 'like', '%' . $this->filters['race_nik'] . '%');
-                    }
+        //             if (!empty($this->filters['race_nik'])) {
+        //                 $racer->where('nik', 'like', '%' . $this->filters['race_nik'] . '%');
+        //             }
 
-                    if (!empty($this->filters['race_city'])) {
-                        $racer->where('hometown', 'like', '%' . $this->filters['race_city'] . '%');
-                    }
+        //             if (!empty($this->filters['race_city'])) {
+        //                 $racer->where('hometown', 'like', '%' . $this->filters['race_city'] . '%');
+        //             }
 
-                    if (($this->filters['race_has_photo'] ?? '') === '1') {
-                        $racer->whereNotNull('photo');
-                    }
+        //             if (($this->filters['race_has_photo'] ?? '') === '1') {
+        //                 $racer->whereNotNull('photo');
+        //             }
 
-                    if (($this->filters['race_has_photo'] ?? '') === '0') {
-                        $racer->whereNull('photo');
-                    }
+        //             if (($this->filters['race_has_photo'] ?? '') === '0') {
+        //                 $racer->whereNull('photo');
+        //             }
 
-                    if (($this->filters['race_has_kis'] ?? '') === '1') {
-                        $racer->whereNotNull('kis');
-                    }
+        //             if (($this->filters['race_has_kis'] ?? '') === '1') {
+        //                 $racer->whereNotNull('kis');
+        //             }
 
-                    if (($this->filters['race_has_kis'] ?? '') === '0') {
-                        $racer->whereNull('kis');
-                    }
+        //             if (($this->filters['race_has_kis'] ?? '') === '0') {
+        //                 $racer->whereNull('kis');
+        //             }
 
-                    if (($this->filters['race_has_kta'] ?? '') === '1') {
-                        $racer->whereNotNull('kta');
-                    }
+        //             if (($this->filters['race_has_kta'] ?? '') === '1') {
+        //                 $racer->whereNotNull('kta');
+        //             }
 
-                    if (($this->filters['race_has_kta'] ?? '') === '0') {
-                        $racer->whereNull('kta');
-                    }
-                });
-            }
-        );
+        //             if (($this->filters['race_has_kta'] ?? '') === '0') {
+        //                 $racer->whereNull('kta');
+        //             }
+        //         });
+        //     }
+        // );
 
-        $query->when($this->filters['race_team_name'] ?? null, function ($q, $value) {
+        // $query->when($this->filters['race_team_name'] ?? null, function ($q, $value) {
 
-            $q->whereHas('registration', function ($registration) use ($value) {
-                $registration->where('team_name', 'like', "%{$value}%");
-            });
+        //     $q->whereHas('registration', function ($registration) use ($value) {
+        //         $registration->where('team_name', 'like', "%{$value}%");
+        //     });
 
-        });
+        // });
 
-        $query->when($this->filters['race_class_name'] ?? null,
-            function ($q, $value) {
+        // $query->when($this->filters['race_class_name'] ?? null,
+        //     function ($q, $value) {
 
-                $q->whereHas('eventClass', function ($class) use ($value) {
-                    $class->where('name', 'like', "%{$value}%");
-                });
+        //         $q->whereHas('eventClass', function ($class) use ($value) {
+        //             $class->where('name', 'like', "%{$value}%");
+        //         });
 
-            });
+        //     });
 
-        $duplicateNumbers = DB::table('registration_classes')
-            ->join('registrations', 'registration_classes.registration_id', '=', 'registrations.id')
-            ->where('registration_classes.event_id', $eventId)
-            ->where('registrations.race_status', 'approved')
-            ->whereNotNull('registration_classes.racer_number')
-            ->groupBy('registration_classes.racer_number')
-            ->havingRaw('COUNT(DISTINCT registrations.racer_id) > 1')
-            ->pluck('registration_classes.racer_number')
-            ->toArray();
+        // $duplicateNumbers = DB::table('registration_classes')
+        //     ->join('registrations', 'registration_classes.registration_id', '=', 'registrations.id')
+        //     ->where('registration_classes.event_id', $eventId)
+        //     ->where('registrations.race_status', 'approved')
+        //     ->whereNotNull('registration_classes.racer_number')
+        //     ->groupBy('registration_classes.racer_number')
+        //     ->havingRaw('COUNT(DISTINCT registrations.racer_id) > 1')
+        //     ->pluck('registration_classes.racer_number')
+        //     ->toArray();
 
-        if (($this->filters['race_racer_number_duplicate'] ?? '') === 'duplicate') {
-            $query->whereIn('racer_number', $duplicateNumbers);
-        }
+        // if (($this->filters['race_racer_number_duplicate'] ?? '') === 'duplicate') {
+        //     $query->whereIn('racer_number', $duplicateNumbers);
+        // }
 
-        if (($this->filters['race_racer_number_duplicate'] ?? '') === 'unique') {
-            $query->whereNotIn('racer_number', $duplicateNumbers);
-        }
+        // if (($this->filters['race_racer_number_duplicate'] ?? '') === 'unique') {
+        //     $query->whereNotIn('racer_number', $duplicateNumbers);
+        // }
 
         return $query->get();
     }
@@ -170,7 +170,7 @@ class RegistrationClassExport implements FromCollection, WithHeadings, WithMappi
         $eventClass = $registClass->eventClass;
 
         return [
-            $registClass->invoice_number,
+            $registration->registration_number,
 
             $racer?->full_name,
             $racer?->short_name,
